e25d2d5f489c65a4d2d1e8f6124cd263c1bb7735
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-02-12 21:40:30 -0300

    Algoritmo que leia os elementos de uma matriz inteira 10x10 e imprima os elementos da matriz. a) Calcule e mostre a soma e a média da diagonal principal / Calcule e mostre a soma e a média da diagonal secundária / Criar um algoritmo que leia os elementos de uma matriz inteira 10x10. Some todos os valores da matriz.

---
+++ CodigoH.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace PATrilhasAtiv07
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		int diagonalprincipal = 0;
+		int diagonalsecundaria = 0;	
+		double mediaprincipal = 0;
+		double mediasecundaria = 0;
+		
+		Random rand = new Random();	
+    	int [,] matriz10 = new int[10, 10];
+    	
+    	for (int i = 0; i < 10; i++) 
+    	{
+    		for (int j = 0; j < 10; j++) 
+    		{
+    			matriz10[i, j] = rand.Next(0, 9);
+    			Console.Write(matriz10[i, j] + " |");
+    		}
+    		Console.WriteLine();
+    	}
+    	Console.WriteLine("\n\rElementos diagonais da matriz: ");
+      
+    	for (int i = 0; i < 10; i++)
+    	{
+    		diagonalprincipal += matriz10[i, i];
+    	}   
+    	
+    	for (int i = 0; i < 10; i++)
+    	{
+    		diagonalsecundaria += matriz10[i, 9 - i];
+    	} 
+    	
+		mediaprincipal = diagonalprincipal / 10;  
+  		mediasecundaria = diagonalsecundaria / 10;
+    	
+    	Console.WriteLine("A soma da diagonal principal é {0} e sua média é {1:F2}.", diagonalprincipal, mediaprincipal);
+    	Console.WriteLine("A soma da diagonal secundaria é {0} e sua média é {1:F2}.", diagonalsecundaria, mediasecundaria);
+		Console.ReadKey(true);
+		}
+
+	}
+
+}
